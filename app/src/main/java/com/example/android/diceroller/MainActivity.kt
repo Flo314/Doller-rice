b33ece9ca7b5239d92722f31e3edf8848f4358c5
“Florent <f.losada@hotmail.fr>
2019-02-24 20:27:57 +0100

    au click : obtenir un nombre aléatoire compris entre 1 et 6

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +20,15 @@ class MainActivity : AppCompatActivity() {
 
         // ajout d'un écouteru de click
         rollButton.setOnClickListener{
-            // afficher par le bouton un message Toast.(3 args context, text, durée)
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
+            //  obtenir un nombre aléatoire compris entre 1 et 6
+            rollDice();
         }
+    }
+
+    // au click : obtenir un nombre aléatoire compris entre 1 et 6
+    private fun rollDice() {
+        val resultText : TextView = findViewById(R.id.result_text);
+        val randomInt = java.util.Random().nextInt(6) + 1;
+        resultText.text = randomInt.toString();
     }
 }
