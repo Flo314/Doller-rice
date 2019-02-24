3a47f7e210ee2f5061d9e06575e1a9c59e8c7587
“Florent <f.losada@hotmail.fr>
2019-02-24 20:44:29 +0100

    Ajouter les images de dés dans le dossier pouvant être dessiné

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -25,10 +26,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // au click : obtenir un nombre aléatoire compris entre 1 et 6
+    // Choisir la bonne ressource pouvant être dessinée en fonction de la valeur de randomInt
     private fun rollDice() {
-        val resultText : TextView = findViewById(R.id.result_text);
         val randomInt = java.util.Random().nextInt(6) + 1;
-        resultText.text = randomInt.toString();
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage : ImageView = findViewById(R.id.dice_image);
+        // affecter la ressource drawableResource d’en haut à diceImage
+        diceImage.setImageResource(drawableResource);
     }
 }
